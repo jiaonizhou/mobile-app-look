654885ef9366e8be1284f17a7edc780d2cded0dd
jiaonizhou <zhoujiaoni14@gmail.com>
2016-06-08 20:33:12 -0700

    commit

--- app/src/main/java/mobile/mycloset/TodayFragment.java
+++ app/src/main/java/mobile/mycloset/TodayFragment.java
@@ -3,6 +3,7 @@ package mobile.mycloset;
 import android.app.Activity;
 import android.app.Fragment;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -11,13 +12,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.content.Intent;
 
 import org.json.JSONObject;
 
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.IOException;
 
 import mobile.mycloset.model.Apparel;
@@ -55,6 +59,7 @@ public class TodayFragment extends Fragment {
     }
 
     public void initTodayView(View view) {
+        final GridLayout suiteView = (GridLayout)view.findViewById(R.id.include);
         final ImageView weatherImage = (ImageView)view.findViewById(R.id.weather);
         final TextView tempView = (TextView)view.findViewById(R.id.temperature);
         final ImageView bagView = (ImageView)view.findViewById(R.id.bag_placeholder);
@@ -62,6 +67,7 @@ public class TodayFragment extends Fragment {
         final ImageView topView = (ImageView)view.findViewById(R.id.top_placeholder);
         final ImageView bottomView = (ImageView)view.findViewById(R.id.bottom_placeholder);
         final Button favButton = (Button)view.findViewById(R.id.fav);
+        final Button shareButton = (Button)view.findViewById(R.id.share);
         final MainActivity activity = (MainActivity)getActivity();
         try {
             Call weatherInfoCall = Utils.getWeatherCall(new Callback() {
@@ -163,6 +169,27 @@ public class TodayFragment extends Fragment {
                                     public void onClick(View view) {
                                         Closet.getCloset().favSuite(suite).save(activity);
 
+                                    }
+                                });
+
+                                shareButton.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        // Create the new Intent using the 'Send' action.
+                                        Intent share = new Intent(Intent.ACTION_SEND);
+
+                                        // Set the MIME type
+                                        share.setType("image/*");
+
+                                        // Create the URI from the media
+                                        File suiteImageFile = Utils.captureView(suiteView, "today_suite");
+                                        Uri uri = Uri.fromFile(suiteImageFile);
+
+                                        // Add the URI to the Intent.
+                                        share.putExtra(Intent.EXTRA_STREAM, uri);
+
+                                        // Broadcast the Intent.
+                                        startActivity(Intent.createChooser(share, "Share to"));
                                     }
                                 });
                             }
--- app/src/main/java/mobile/mycloset/utils/Utils.java
+++ app/src/main/java/mobile/mycloset/utils/Utils.java
@@ -1,7 +1,14 @@
 package mobile.mycloset.utils;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Environment;
+import android.view.View;
+
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import mobile.mycloset.model.WeatherParser;
@@ -26,5 +33,27 @@ public class Utils {
         Call call = client.newCall(request);
         call.enqueue(callback);
         return call;
+    }
+
+    public static File captureView(View view,String filename){
+        //Create a Bitmap with the same dimensions
+        Bitmap image = Bitmap.createBitmap(view.getWidth(),
+                view.getHeight(),
+                Bitmap.Config.RGB_565);
+        //Draw the view inside the Bitmap
+        view.draw(new Canvas(image));
+
+        //Store to sdcard
+        try {
+            String path = Environment.getExternalStorageDirectory().toString();
+            File myFile = new File(path,filename);
+            FileOutputStream out = new FileOutputStream(myFile);
+
+            image.compress(Bitmap.CompressFormat.PNG, 90, out); //Output
+            return myFile;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
